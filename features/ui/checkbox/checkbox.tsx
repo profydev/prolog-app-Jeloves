aeac67092d0c309eb32abbdb07667800d183ee59
Jeloves <anjelovaleraecube@icloud.com>
2024-03-08 11:51:11 -0500

    Aded styles for sizes, state, and labels

--- features/ui/checkbox/checkbox.tsx
+++ features/ui/checkbox/checkbox.tsx
@@ -6,29 +6,47 @@ export type CheckboxPropsType = {
   label: LabelHTMLAttributes<HTMLLabelElement>;
   input: InputHTMLAttributes<HTMLInputElement>;
   size: string;
+  text: string;
 };
 
 export function Checkbox(props: CheckboxPropsType) {
-  const { label, input, size } = props;
+  const { label, input, size, text } = props;
 
   const labelClasses = {
     [styles.label]: true,
     [styles[size]]: size,
   };
 
+  const spanClasses = {
+    [styles.labelText]: true,
+    [styles[size]]: size,
+  };
+
+  const labelEl = (
+    <label
+      {...label}
+      htmlFor="checkboxID"
+      className={classNames(labelClasses)}
+    ></label>
+  );
+  const spanEl = <span className={classNames(spanClasses)}>{text}</span>;
+
   return (
-    <>
+    <div className={styles.checkboxContainer}>
       <input
         {...input}
         type="checkbox"
         id="checkboxID"
         className={styles.input}
       />
-      <label
-        {...label}
-        htmlFor="checkboxID"
-        className={classNames(labelClasses)}
-      ></label>
-    </>
+      {text ? (
+        <>
+          {labelEl}
+          {spanEl}
+        </>
+      ) : (
+        <>{labelEl}</>
+      )}
+    </div>
   );
 }
