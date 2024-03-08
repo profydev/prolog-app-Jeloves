b6b523bd02bb95e2746ee27b42de80e910f00269
Jeloves <anjelovaleraecube@icloud.com>
2024-03-08 10:04:58 -0500

    Created checkbox component with configurable size

--- features/ui/checkbox/checkbox.tsx
+++ features/ui/checkbox/checkbox.tsx
@@ -1,3 +1,34 @@
-export function Checkbox() {
-  return <input type="checkbox"></input>;
+import { InputHTMLAttributes, LabelHTMLAttributes } from "react";
+import classNames from "classnames";
+import styles from "./checkbox.module.scss";
+
+export type CheckboxPropsType = {
+  label: LabelHTMLAttributes<HTMLLabelElement>;
+  input: InputHTMLAttributes<HTMLInputElement>;
+  size: string;
+};
+
+export function Checkbox(props: CheckboxPropsType) {
+  const { label, input, size } = props;
+
+  const labelClasses = {
+    [styles.label]: true,
+    [styles[size]]: size,
+  };
+
+  return (
+    <>
+      <input
+        {...input}
+        type="checkbox"
+        id="checkboxID"
+        className={styles.input}
+      />
+      <label
+        {...label}
+        htmlFor="checkboxID"
+        className={classNames(labelClasses)}
+      ></label>
+    </>
+  );
 }
