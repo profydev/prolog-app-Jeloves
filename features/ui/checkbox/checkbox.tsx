5f7e8a9ae3ec2472ee8077ae7be6c5f275e265ee
Jeloves <anjelovaleraecube@icloud.com>
2024-03-11 10:57:19 -0400

    Added three-state functionality

--- features/ui/checkbox/checkbox.tsx
+++ features/ui/checkbox/checkbox.tsx
@@ -1,4 +1,9 @@
-import { InputHTMLAttributes, LabelHTMLAttributes } from "react";
+import {
+  InputHTMLAttributes,
+  LabelHTMLAttributes,
+  useRef,
+  useState,
+} from "react";
 import classNames from "classnames";
 import styles from "./checkbox.module.scss";
 
@@ -12,18 +17,39 @@ export type CheckboxPropsType = {
 export function Checkbox(props: CheckboxPropsType) {
   const { label, input, size, text } = props;
 
+  const [status, setStatus] = useState<
+    "checked" | "indeterminate" | "unchecked"
+  >("unchecked");
+  const inputReference = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    switch (status) {
+      case "unchecked":
+        setStatus("checked");
+        break;
+      case "checked":
+        setStatus("indeterminate");
+        inputReference.current!.indeterminate = true;
+        break;
+      case "indeterminate":
+        setStatus("unchecked");
+        inputReference.current!.indeterminate = false;
+        break;
+    }
+  };
+
   const labelClasses = {
     [styles.label]: true,
     [styles[size]]: size,
+    [styles[status]]: status,
   };
-
   const labelTextElement = <span className={styles.labelText}>{text}</span>;
-
+  console.log(status);
   // When :disabling this component, only <input> must be disabled.
   return (
     <>
       <input
         {...input}
+        ref={inputReference}
         type="checkbox"
         id="checkboxID"
         className={styles.input}
@@ -32,6 +58,7 @@ export function Checkbox(props: CheckboxPropsType) {
         {...label}
         htmlFor="checkboxID"
         className={classNames(labelClasses)}
+        onClick={onClick}
       >
         {text !== null && <>{labelTextElement}</>}
       </label>
