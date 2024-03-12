802d79723ca183726b6f68493f3d90d390207bf5
Jeloves <anjelovaleraecube@icloud.com>
2024-03-12 14:56:33 -0400

    Completed the rest of figma designs

--- features/ui/select/select.stories.tsx
+++ features/ui/select/select.stories.tsx
@@ -16,7 +16,18 @@ const Template: StoryFn<SelectPropsType> = (args) => <Select {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
+  options: [
+    "Phoenix Baker",
+    "Olivia Rhye",
+    "Lana Steiner",
+    "Demi Wilkinson",
+    "Candice Wu",
+    "Natali Craig",
+    "Drew Cano",
+  ],
+  emptySelectionText: "Select team member",
   icon: "/icons/user.svg",
   label: "Team member",
   hint: "This is a hint text to help user.",
+  error: "This is an error message",
 };
--- features/ui/select/select.tsx
+++ features/ui/select/select.tsx
@@ -1,18 +1,51 @@
+import classNames from "classnames";
 import styles from "./select.module.scss";
+import { useState } from "react";
 
 // icon, label, hint, error
 export type SelectPropsType = {
+  options: string[];
+  emptySelectionText: string;
   icon: string | null;
   label: string | null;
   hint: string | null;
+  error: string | null;
 };
 
 export function Select(props: SelectPropsType) {
-  const { icon, label, hint } = props;
+  const { options, emptySelectionText, icon, label, hint, error } = props;
+
+  const [isShowingOptions, setIsShowingOptions] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(emptySelectionText);
+
+  const handleShowOptionsClick = () => {
+    setIsShowingOptions(!isShowingOptions);
+  };
+
+  const handleSelectOptionClick = (index: number) => {
+    const clickedOption: string = options[index];
+    if (selectedOption === clickedOption) {
+      setSelectedOption(emptySelectionText);
+    } else {
+      setSelectedOption(clickedOption);
+    }
+  };
+
+  const selectElement = (
+    <select className={styles.select} id="selectID">
+      <>
+        {options.map((opt, index) => (
+          <option key={index} value={opt}>
+            {opt}
+          </option>
+        ))}
+      </>
+    </select>
+  );
 
   const iconElement = (
     // eslint-disable-next-line @next/next/no-img-element
-    <img className={styles.icon} alt="icon" src={icon ? icon : ""}></img>
+    <img alt="icon" src={icon ? icon : ""}></img>
   );
 
   const labelElement = (
@@ -21,30 +54,78 @@ export function Select(props: SelectPropsType) {
     </label>
   );
 
-  const hintElement = <span className={styles.hint}>{hint}</span>;
+  const hintElement = error ? (
+    <span className={classNames(styles.hint, styles.error)}>{error}</span>
+  ) : (
+    <span className={styles.hint}>{hint}</span>
+  );
 
-  const handleOnClick = () => {
-    alert("hello");
+  const selectContentClasses = {
+    [styles.selectContent]: true,
+    [styles.error]: error ? true : false,
   };
+
+  const selectContentElement = (
+    <div
+      className={classNames(selectContentClasses)}
+      onClick={handleShowOptionsClick}
+    >
+      {icon !== null && iconElement}
+      <span
+        className={classNames(
+          styles.selectText,
+          selectedOption === emptySelectionText && styles.empty,
+        )}
+      >
+        {selectedOption}
+      </span>
+      {/*eslint-disable-next-line @next/next/no-img-element*/}
+      <img
+        alt="drop-down button"
+        src={
+          isShowingOptions ? "/icons/chevron-up.svg" : "/icons/chevron-down.svg"
+        }
+      />
+    </div>
+  );
+
+  const optionsContainer = (
+    <div className={styles.optionsContainer}>
+      {options.map((opt, index) => (
+        <div
+          key={index}
+          className={
+            selectedOption === opt
+              ? classNames(styles.optionsContent, styles.optionSelected)
+              : styles.optionsContent
+          }
+          onClick={() => {
+            handleSelectOptionClick(index);
+          }}
+        >
+          {icon !== null && iconElement}
+          <span key={index} className={styles.option}>
+            {opt}
+          </span>
+          {selectedOption === opt && (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img alt="option selected" src="/icons/check-thin.svg"></img>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+
   return (
-    <div className={styles.container}>
+    <div className={styles.selectContainer}>
       {label !== null && labelElement}
-      <div className={styles.content} onClick={handleOnClick}>
-        {icon !== null && iconElement}
-        <span className={styles.text}>Select team member</span>
-        {/*eslint-disable-next-line @next/next/no-img-element*/}
-        <img
-          className={styles.dropdownButton}
-          alt="drop-down button"
-          src="/icons/chevron-down.svg"
-        ></img>
-      </div>
-      {icon !== null && hintElement}
-      <select className={styles.select} id="selectID">
-        <option value="blue"></option>
-        <option value="red"></option>
-        <option value="green"></option>
-      </select>
+      {selectElement}
+      {selectContentElement}
+      {isShowingOptions && optionsContainer}
+
+      {/* If a hint || error exists, will display the hint element */}
+      {/* UNLESS the select-options are showing */}
+      {(hint !== null || error !== null) && !isShowingOptions && hintElement}
     </div>
   );
 }
